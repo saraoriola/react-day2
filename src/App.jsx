c4832359ec1dae07a3c52d1f8b83b67239d1c8fa
saraoriola <saraoriola@outlook.com>
2023-07-26 17:52:38 +0200

    actualizado ejercicio 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
-import CounterFunctional from './components/Counter/CounterFunctional.jsx';
+import CounterFunctional from './components/Counter/CounterFunctional';
 
 const App = () => {
   return (
     <div>
-      <CounterFunctional initialValue={0}/>
+      <h1>Counter</h1>
+      <CounterFunctional initialValue={0} number={2}/>
+      <CounterFunctional initialValue={3} number={3}/>
+      <CounterFunctional initialValue={10} number={10}/>
     </div>
   );
 };
--- src/components/Counter/CounterFunctional.jsx
+++ src/components/Counter/CounterFunctional.jsx
@@ -3,21 +3,23 @@ import "./CounterFunctional.css";
 
 const CounterFunctional = (props) => {
   const [counter, setCounter] = useState(props.initialValue);
+  const {number} = props; 
 
-  const increment = () => {
-    setCounter(counter + 1);
-  };
+  const increment = () => setCounter(counter + number);
+  const decrement = () => setCounter(counter - number);
 
-  const decrement = () => {
-    setCounter(counter - 1);
-  };
 
   return (
+    <>
     <div className="calculator">
+
       <button className="calculator__button" onClick={decrement}>-</button>
       <span className="calculator__display">{counter}</span>
       <button className="calculator__button" onClick={increment}>+</button>
+
     </div>
+    </>  
+    
   );
 };
 
