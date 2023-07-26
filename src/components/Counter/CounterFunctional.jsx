0f5656f105cefba2953f9b6b3f8eb714de9f8ae9
saraoriola <saraoriola@outlook.com>
2023-07-26 18:16:28 +0200

    retocando css counter

--- src/components/Counter/CounterFunctional.jsx
+++ src/components/Counter/CounterFunctional.jsx
@@ -3,23 +3,23 @@ import "./CounterFunctional.css";
 
 const CounterFunctional = (props) => {
   const [counter, setCounter] = useState(props.initialValue);
-  const {number} = props; 
+  const { number } = props;
 
   const increment = () => setCounter(counter + number);
   const decrement = () => setCounter(counter - number);
 
-
   return (
     <>
-    <div className="calculator">
-
-      <button className="calculator__button" onClick={decrement}>-</button>
-      <span className="calculator__display">{counter}</span>
-      <button className="calculator__button" onClick={increment}>+</button>
-
-    </div>
-    </>  
-    
+      <div className="calculator">
+        <button className="calculator__button calculator__button-minus" onClick={decrement}>
+          -
+        </button>
+        <span className="calculator__display">{counter}</span>
+        <button className="calculator__button calculator__button-plus" onClick={increment}>
+          +
+        </button>
+      </div>
+    </>
   );
 };
 
